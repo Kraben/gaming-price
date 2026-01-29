083866be1b132390c6b8ab259656b558dafa716a
Kraben <alegs@live.com>
2026-01-29 11:51:07 -0600

    Update amazon.js

--- api/amazon.js
+++ api/amazon.js
@@ -3,8 +3,7 @@ module.exports = async function handler(req, res) {
   const API_KEY = process.env.RAINFOREST_KEY;
 
   if (!query) return res.status(400).json({ error: 'Query requerida' });
-const url = `https://api.rainforestapi.com/request?api_key=${API_KEY}&type=search&amazon_domain=amazon.com.mx&search_term=${encodeURIComponent(query)}&exclude_sponsored=true`;
-  try {
+const url = `https://api.rainforestapi.com/request?api_key=${API_KEY}&type=search&amazon_domain=amazon.com.mx&search_term=${encodeURIComponent(query)}&category_id=bestsellers_videogames&exclude_sponsored=true`;  try {
     const response = await fetch(url);
     const data = await response.json();
 
